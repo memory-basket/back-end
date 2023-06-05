5909f3003c9f835c9e05b726453cdab70d3d3b11
wonjunYou <youwjune@gmail.com>
2023-06-06 03:08:56 +0900

    feat : 초대 링크 조회 기능 구현

--- src/main/java/com/flower/auth/CustomOAuth2UserService.java
+++ src/main/java/com/flower/auth/CustomOAuth2UserService.java
@@ -2,21 +2,18 @@ package com.flower.auth;
 
 import com.flower.auth.userInfo.KakaoUserInfo;
 import com.flower.auth.userInfo.OAuth2UserInfo;
-import com.flower.member.Member;
-import com.flower.member.MemberRepository;
-import com.flower.member.MemberService;
+import com.flower.member.domain.Member;
+import com.flower.member.repository.MemberRepository;
+import com.flower.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.servlet.http.HttpSession;
 import java.util.UUID;
 
 @Service
--- src/main/java/com/flower/member/repository/MemberRepository.java
+++ src/main/java/com/flower/member/repository/MemberRepository.java
@@ -1,5 +1,6 @@
-package com.flower.member;
+package com.flower.member.repository;
 
+import com.flower.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
